d31c908010b16e8b154aefe2e489ce190af569b5
Eman_Elsayed <112521958+EmanElsayed2002@users.noreply.github.com>
2025-01-15 10:34:07 +0200

    Add files via upload

---
+++ Business Logic Layer/Entities/Dept_Ins.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI_Examination
+{
+    public class Dept_Ins
+    {
+        public int Dept_ID { get; set; }
+        public int Ins_ID { get; set; }
+    }
+}
---
+++ Business Logic Layer/Entities/Exam_Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI_Examination
+{
+    public class Exam_Question
+    {
+        public int ExamID { get; set; }
+        public int QuestionID { get; set; }
+    }
+}
